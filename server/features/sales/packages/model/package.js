86db6f6587082998d8158c9153150501fe951a0c
Ibrahim Shoeib <ibrahimshoeib255@gmail.com>
2024-06-27 02:12:33 +0300

    Update package.js

--- server/features/sales/packages/model/package.js
+++ server/features/sales/packages/model/package.js
@@ -14,7 +14,7 @@ const packageSchema = mongoose.Schema({
     required: true,
   },
   lessons: {
-    type: Number,
+    type: String,
     enum:["1","2","3","4","5","6","7","8","9","10"],
     required: true,
   },
@@ -38,7 +38,7 @@ const Package = mongoose.model("Package", packageSchema);
 function createNewPackage(obj) {
   const schema = joi.object({
     category:joi.string().required().min(1).max(20).valid("basic","novice","advance","junior team"),
-    lessons:joi.number().required().min(1).max(20).valid("1","2","3","4","5","6","7","8","9","10"),
+    lessons:joi.srting().required().min(1).max(20).valid("1","2","3","4","5","6","7","8","9","10"),
     startDate:joi.string().required().min(1).max(20),
     endDate:joi.string().required().min(1).max(20),
     status:joi.string().required().valid("expired","active").min(1).max(20),
@@ -50,7 +50,7 @@ function createNewPackage(obj) {
 function updatePackage(obj) {
   const schema = joi.object({
     category:joi.string().required().min(1).max(20).valid("basic","novice","advance","junior team"),
-    lessons:joi.number().required().min(1).max(20).valid("1","2","3","4","5","6","7","8","9","10"),
+    lessons:joi.string().required().min(1).max(20).valid("1","2","3","4","5","6","7","8","9","10"),
     startDate:joi.string().required().min(1).max(20),
     endDate:joi.string().required().min(1).max(20),
     status:joi.string().required().valid("expired","active").min(1).max(20),
