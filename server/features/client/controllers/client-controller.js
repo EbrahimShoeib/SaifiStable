70cc3f7a11bac1793a0da0e4259343b516b13763
ibrahimshoeib <hsho2214@gmail.com>
2024-06-30 14:13:21 +0300

    chore

--- server/features/client/controllers/client-controller.js
+++ server/features/client/controllers/client-controller.js
@@ -87,7 +87,7 @@ class ClientController {
   static async getClientById(req, res) {
     try {
       Client.findById(req.params.id)
-      Client.populate("hourseId")
+      // .populate("hourseId")
         .then((docs) => {
           if (docs) {
             const { __v, ...other } = docs._doc;
